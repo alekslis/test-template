a58738a6f94242509d7689518699c44888043b2c
alekslis <aleks_lis@tutanota.com>
2019-01-09 21:45:46 +0100

    kold

--- prog-template.c
+++ prog-template.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <math.h>
 #include <unistd.h>
-//testgitawcode
+//chybasieudalo
 static knet_dev_t * dsPic; // robot pic microcontroller access
 int maxsp,accinc,accdiv,minspacc, minspdec; // for speed profile
 static int quitReq = 0; // quit variable for loop
